a3d27f0091c56aaa8ce9da08ca4fb37afab52e17
Grzegorz Ziemski <ziemski@gmail.com>
2025-01-09 12:26:40 +0100

    Initial projects generated by Android Studio

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dynamic Pager for Android Jetpack Compose"
+include(":sample-app")
+include(":dynamic-pager")
